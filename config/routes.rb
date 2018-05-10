f3f7264a8380c504046aae94c87b45ba6db39762
Sean Oughton <seanoughton@gmail.com>
2018-05-10 22:41:40 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :preferences
+  #resources :preferences
   resources :artists do
     resources :songs, only: [:index, :show]
   end
   resources :songs
+
+  scope '/admin', module: 'admin' do
+    resources :preferences, only: [:index]
+  end
 
 end
